6e8860b9c06afbf549e2e47af85245e11c2a59d6
Kyle Mulqueen <kmulqdrums710@gmail.com>
2025-05-09 22:57:35 -0700

    created basic text field component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Button from "./components/Button";
+import TextField from "./components/TextField";
 
 function App() {
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>): void => {
-    console.log("Button clicked:", e.target);
+  const handleClick = (): void => {
+    console.log("Generate password");
   };
   return (
     <main className="font-jetBrains">
       <h1 className="text-grey-600 mb-4 text-center text-preset-4">
         Password Generator
       </h1>
+      <TextField />
       <Button onClick={handleClick}>GENERATE</Button>
     </main>
   );
